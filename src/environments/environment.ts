97479a3045fdaacf29de77c57083e8e668d629af
fazim-developer <fazim.developer@gmail.com>
2020-08-09 17:04:13 +0530

    🚀 Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAY1uIb_W5JaF0C_bu4Ws6t4MmKeAdhh3c",
+    authDomain: "aagneya-dash.firebaseapp.com",
+    databaseURL: "https://aagneya-dash.firebaseio.com",
+    projectId: "aagneya-dash",
+    storageBucket: "aagneya-dash.appspot.com",
+    messagingSenderId: "679937650036",
+    appId: "1:679937650036:web:cec1f6dbc3e4b864e9d021",
+    measurementId: "G-REK2TFZF9R"
+  }
 };
 
 /*
